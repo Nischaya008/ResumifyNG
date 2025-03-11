e58df6b040483610be60e3fc4724d7861eeff86f
Nischaya Garg <nischayagarg008@gmail.com>
2025-03-11 13:38:02 +0530

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -3,7 +3,10 @@ const API_BASE_URL = 'https://resumifyng-backend.up.railway.app'
 export const endpoints = {
   uploadResume: `${API_BASE_URL}/api/upload_resume`,
   generateATS: `${API_BASE_URL}/api/generate_ats`,
-  enhanceResume: `${API_BASE_URL}/api/enhance_resume`
+  enhanceResume: `${API_BASE_URL}/api/enhance_resume`,
+  startInterview: `${API_BASE_URL}/api/start_interview`,
+  sendMessage: `${API_BASE_URL}/api/send_message`,
+  toggleMute: `${API_BASE_URL}/api/toggle_mute`
 }
 
 export default API_BASE_URL
